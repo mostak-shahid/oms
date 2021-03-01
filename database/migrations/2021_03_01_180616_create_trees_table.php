a6a463472015d8ef00ab15ef89abbef77d4facf4
Md. Mostak Shahid <mostak.shahid@gmail.com>
2021-03-01 18:16:56 +0600

    Update 2021_03_01_180616_create_trees_table.php

--- database/migrations/2021_03_01_180616_create_trees_table.php
+++ database/migrations/2021_03_01_180616_create_trees_table.php
@@ -15,6 +15,8 @@ class CreateTreesTable extends Migration
     {
         Schema::create('trees', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id');
+            $table->foreignId('child_id');
             $table->timestamps();
         });
     }
